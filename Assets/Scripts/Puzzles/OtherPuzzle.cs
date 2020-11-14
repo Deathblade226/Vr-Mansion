83f14fe1777b0cf2ef5a04060ca507ae1b2a271d
Skota18855 <47341526+Skota18855@users.noreply.github.com>
2020-11-13 17:01:29 -0700

    Added basic scripts from uml except pickup and player controller

---
+++ Assets/Scripts/Puzzles/OtherPuzzle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherPuzzle : Puzzle
+{
+    public override void CheckSolution()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SolutionAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Puzzles/PlacementPuzzle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementPuzzle : Puzzle
+{
+    [SerializeField] private GameObject[] items;
+    [SerializeField] private GameObject[] locations;
+    public override void CheckSolution()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void SolutionAction()
+    {
+        throw new System.NotImplementedException();
+    }
+}

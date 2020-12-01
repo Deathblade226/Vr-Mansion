65697aacd2fd88b7ed0d348273949b0505b8bc11
Skota18855 <47341526+Skota18855@users.noreply.github.com>
2020-11-30 19:27:44 -0700

    Finished Fireplace puzzle and modifies puzzle script slightly

--- Assets/Scripts/Puzzles/Puzzle.cs
+++ Assets/Scripts/Puzzles/Puzzle.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class Puzzle : MonoBehaviour
 {
+    [System.Serializable]
+    public class InteractionEvent : UnityEvent { }
+
+    public InteractionEvent onPuzzleSolved = new InteractionEvent();
+
     public abstract bool CheckSolution();
     public abstract void SolutionAction();
 }
